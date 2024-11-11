e3f680f17667ce1656f9def7d38133499d3cd226
Daria <darjana29@gmail.com>
2024-11-11 23:47:13 +0300

    Update numbers-tasks.js

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -109,8 +109,8 @@ function getLinearEquationRoot(a, b) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  return x1 * x2 + y1 * y2;
 }
 
 /**
@@ -203,8 +203,16 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+  for (let i = 2; i <= n / 2; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
